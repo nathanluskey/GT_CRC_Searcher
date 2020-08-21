fc727a324a578505ca552d0ea625571905b6700f
nathanluskey <34701768+nilnate@users.noreply.github.com>
2020-08-21 11:41:20 -0400

    Added basic notifications

--- SimpleVersion/main.py
+++ SimpleVersion/main.py
@@ -4,9 +4,32 @@ from secrets import secrets
 from time import sleep
 
 def loadCRC():
-    rawText = requests.get(CRC_URL).text
-    print(rawText)
+    """Loads the CRC Webpage
 
+    Returns
+    -------
+    rawText
+        the raw HTML text of the webpage
+    """
+    rawText = requests.get(CRC_URL).text
+    # TODO: Use bs4 to look format text into something useable
+    # print(rawText)
+    return rawText
+
+def sendNotification(message, verbose=False):
+    """Send out notification to all users via HTTPS Requests & Telegram API
+
+    Parameters
+    ----------
+    message : str
+        The message to be sent in the chat
+    verbose : bool, optional
+        Default False. For printing out useful information.
+    """
+    URL = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}&parse_mode=Markdown".format(secrets.CHAT_BOT_TOKEN, secrets.CHANNEL_ID, message)
+    response = requests.get(URL)
+    if verbose:
+        print("URL:\n\t{0}\nResponse:\n\t{1}".format(URL, response))
 
 if __name__ == "__main__":
     # I made a bitly link to shorten CRC 1st floor
@@ -19,6 +42,6 @@ if __name__ == "__main__":
         NewCRCOpenings = loadCRC()
         #TODO: Check if there are changes over the last check
         if not(NewCRCOpenings == CRCOpenings):
-            #TODO: Send out notification to all users via HTTPS Requests & Telegram API
-            pass
+            message = "Hello\nWorld!"
+            sendNotification(message, verbose=True)
         keepRunning = False
