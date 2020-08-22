43b7d94642e397fac9f4e891aa0aa0e72ec3a683
nathanluskey <34701768+nilnate@users.noreply.github.com>
2020-08-22 14:14:25 -0400

    Added messaging and updating

--- SimpleVersion/main.py
+++ SimpleVersion/main.py
@@ -1,20 +1,69 @@
-import requests
+from requests import get
 from bs4 import BeautifulSoup
 from secrets import secrets
 from time import sleep
+from random import randint
 
-def loadCRC():
+def loadCRC(verbose = False):
     """Loads the CRC Webpage
+    Parameters
+    ----------
+    verbose : bool, optional
+        Default False. For printing out useful information.
 
     Returns
     -------
-    rawText
+    openings : dict
         the raw HTML text of the webpage
     """
-    rawText = requests.get(CRC_URL).text
-    # TODO: Use bs4 to look format text into something useable
-    # print(rawText)
-    return rawText
+    rawHTML = get(CRC_URL).text
+    soup = BeautifulSoup(rawHTML, 'html.parser')
+    allTimeCards = soup.find_all("div", class_="caption program-schedule-card-caption")
+    # print("Type: {0}, Object: {1}".format(type(allTimeCards), allTimeCards)) 
+    # Check if there are items
+    openings = dict()
+    if (len(allTimeCards) != 0):
+        for timeCard in allTimeCards:
+            #TODO: Impliment below comments
+                # I think I'll store values in a dict as such:
+                # key - hash the time + date string 
+                # value - number of spots available
+            pass
+    if verbose:
+        print("Total Time Cards = {}".format(len(allTimeCards)))
+    return openings
+
+def checkDifferences(new, old):
+    """ Check the old and new CRC dicts to see if there's been an update
+    
+    Parameters
+    new : dict
+        The most recent CRC openings
+    old : dict
+        The previous CRC openings
+
+    Returns
+    CRCUpdates : str
+        The formatted string in Markdown
+    """
+    CRCUpdates = ""
+    # If there is any update to be made
+    if (new != old):
+        # If the new dict isn't empty
+        CRCUpdates = "UPDATES TO CRC 1ST FLOOR:\n"
+        if (bool(new)):
+            # Check all keys in the new dict for three situations: 1) It's a new key, so a new timeslot opened
+            #                                                      2) It's a new value, so somehow the timeslot changed
+            #                                                      3) Nothing has changed
+            for key in (new.keys()):
+                if key in old:
+                    # Check value to see only if change in number of spots
+                    if (new[key] > old[key]):
+                        CRCUpdates += "New Openings: {0}, {1} --> {2}\n".format(key, old[key], new[key])
+                else:
+                    # Add key to update
+                    CRCUpdates += "New Date: {0}, {1}\n".format(key, new[key])
+    return CRCUpdates
 
 def sendNotification(message, verbose=False):
     """Send out notification to all users via HTTPS Requests & Telegram API
@@ -27,21 +76,31 @@ def sendNotification(message, verbose=False):
         Default False. For printing out useful information.
     """
     URL = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}&parse_mode=Markdown".format(secrets.CHAT_BOT_TOKEN, secrets.CHANNEL_ID, message)
-    response = requests.get(URL)
+    response = get(URL).text
     if verbose:
         print("URL:\n\t{0}\nResponse:\n\t{1}".format(URL, response))
 
 if __name__ == "__main__":
     # I made a bitly link to shorten CRC 1st floor
     CRC_URL = "https://b.gatech.edu/2ErPqzW"
-    DELAY = 5 * 60
-    CRCOpenings = ""
+    DELAY = 5# * 60
+    # CRCOpenings = loadCRC()
+    # For quick debugging using an arbitrary exmaple
+    CRCOpenings = {"one" : 1}
     keepRunning = True
     while keepRunning:
-        #TODO: Load CRC page
-        NewCRCOpenings = loadCRC()
-        #TODO: Check if there are changes over the last check
-        if not(NewCRCOpenings == CRCOpenings):
-            message = "Hello\nWorld!"
+        # Load CRC page
+        # newCRCOpenings = loadCRC(verbose=True)
+        # For quick debugging using an arbitrary update
+        newCRCOpenings = {"one" : 2, "two" : 3}
+        # Check if there are changes over the last check
+        CRCUpdates = checkDifferences(newCRCOpenings, CRCOpenings)
+        if (CRCUpdates != ""):
+            message = "{0}\n[CRC Website](https://b.gatech.edu/2ErPqzW)".format(CRCUpdates)
             sendNotification(message, verbose=True)
-        keepRunning = False
+            # Set new dict to the current openings
+            CRCOpenings = newCRCOpenings
+        # Pause for DELAY amount of seconds +/- 30 seconds
+        sleep(DELAY + randint(-3, 3))
+        # # For debugging only run through the loop once
+        # keepRunning = False
