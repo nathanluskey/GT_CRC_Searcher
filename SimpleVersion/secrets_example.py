07c0262151d7a8c020d678c073d633b92c54ae91
nathanluskey <34701768+nilnate@users.noreply.github.com>
2020-08-24 11:30:19 -0400

    Created to show example

---
+++ SimpleVersion/secrets_example.py
@@ -0,0 +1,19 @@
+class secrets:
+    """
+    A class to store things you don't want publicly on Github
+    For info on how to create and manage a chat bot, look at the Botfather documentation here:
+        https://core.telegram.org/bots
+  
+
+    ...
+
+    Attributes
+    ----------
+    CHAT_BOT_TOKEN : str
+        The token for the bot.
+    CHANNEL_ID : str
+        the channel ID for what groupchat the bot is sending messages to.
+
+    """
+    CHAT_BOT_TOKEN = ""
+    CHANNEL_ID = ""
